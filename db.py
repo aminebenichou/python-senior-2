ec861d29bf07e2b93f6c806ab83ced07c45c29a3
Benichou Mohammed El Amine <31164395+aminebenichou@users.noreply.github.com>
2024-07-29 15:15:32 +0100

    final version of the python senior 2 course

--- db.py
+++ db.py
@@ -1,30 +1,40 @@
 import sqlite3
 
+# SQLite3 is a pre installed library with python
+# you don't need to install it
+# connects or creates database if it does not exist
+# To view SQLite3 db in vs code you need to install the extension SQLite3 Editor from the id under :
+# yy0931.vscode-sqlite3-editor
+# type it in the search bar and it will show up
 connection = sqlite3.connect("mytodo.db")
 
 
 cursor = connection.cursor()
-
+# creating table in database if it does not exist
+# id is auto incremented so we don't need to add when inserting elements/rows
 cursor.execute(
     '''
         CREATE TABLE IF NOT EXISTS todo (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT);
     '''
 )
-
+# inserting element by passing argument item which is a string 
 def insertItem(item):
     cursor.execute(
         f'''
             INSERT INTO todo (title) values ("{item}");
         '''
     )
+    # to save changes made to our table
     connection.commit()
 
-
+# to getall items
+# fetchall converts the result from object to a list containing our rows
 def retrieveItems():
     result = cursor.execute("SELECT * FROM todo").fetchall()
-    print(result)
+    
     return result
 
+# editing items by passing itemid which is an integer and data which is a string
 def editItem(itemid, data):
     cursor.execute(
         f'''
@@ -32,3 +42,15 @@ def editItem(itemid, data):
         '''
     )
     connection.commit()
+
+# deleting items by passing itemid which is an integer
+def deleteItem(itemid):
+    cursor.execute(
+        f'''
+            DELETE FROM todo WHERE id = {itemid}
+        '''
+    )
+    connection.commit()
+
+
+    
--- software.py
+++ software.py
@@ -1,13 +1,20 @@
+# to install pyqt5 run the following command in your terminal
+# pip install PyQt5
+# if you are using vscode press ctrl + J to open terminal
+# from db imports are the functions we created in our db.py file
+# functools is an already installed library with python
+
 from functools import partial
 from PyQt5 import QtWidgets
-from db import insertItem, retrieveItems, editItem
+from db import insertItem, retrieveItems, editItem, deleteItem
 app = QtWidgets.QApplication([])
 
 
 class MainWindow(QtWidgets.QWidget):
     def __init__(self) -> None:
+        # super.init is required
         super().__init__()
-        print(retrieveItems())
+        
         
         self.main_layout = QtWidgets.QHBoxLayout(self)
         self.left_layout = QtWidgets.QVBoxLayout()
@@ -22,10 +29,19 @@ class MainWindow(QtWidgets.QWidget):
         self.left_layout.addWidget(self.title)
         self.left_layout.addWidget(self.add_btn)
         self.display_items()
+    # inserting items to db 
     def add_item(self):
         insertItem(self.title.text())
+        self.refresh()
+        self.title.clear()
+    # editing items from db it requires id and the data you pass data should be a string
     def edit_item(self, itemid):
         editItem(data=self.title.text(), itemid=itemid)
+        self.refresh()
+        self.title.clear()
+
+    # gets items from db and creates buttons with the labels 
+    # we used partial function to make it easy to send params to the connected functions
     def display_items(self):
         items = retrieveItems()
         for item in items:
@@ -33,7 +49,23 @@ class MainWindow(QtWidgets.QWidget):
             self.right_layout.addWidget(QtWidgets.QLabel(item[1]))
             self.right_layout.addWidget(edit_btn)
             edit_btn.clicked.connect(partial(self.edit_item, item[0]))
+            delete_btn = QtWidgets.QPushButton("Delete")
+            self.right_layout.addWidget(delete_btn)
+            delete_btn.clicked.connect(partial(self.delete_item, item[0]))
 
+    # clears all items and displays them again this only works with the right layout
+    def refresh(self):
+        # this for statement counts all of the widgets in the rightlayout
+        for i in reversed(range(self.right_layout.count())):
+            # the line under is to delete eachitem in the right layout 
+            self.right_layout.itemAt(i).widget().deleteLater()
+        
+        self.display_items()
+    # deleting items from db 
+    # the item argument should be an integer
+    def delete_item(self, item):
+        deleteItem(item)
+        self.refresh()
 widget = MainWindow()
 widget.resize(800, 600)
 widget.show()
